e87e2a20dbf98ba2231f0d9e6600b81d0a115d40
Zack Koh <zhenxiang.koh@gmail.com>
2020-03-19 23:04:54 +0800

    Updates

---
+++ precache-manifest.e8341fd2df079a6f7119d75e27854c35.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe8e90f5d2ee0b2dd0e3ae72d6a2b4d0",
+    "url": "/recipe-notebook/index.html"
+  },
+  {
+    "revision": "cb72a48ec2fbb66295d0",
+    "url": "/recipe-notebook/static/css/main.2b89eb3b.chunk.css"
+  },
+  {
+    "revision": "f3e5408bac27325c091f",
+    "url": "/recipe-notebook/static/js/0.8d6ca1b0.chunk.js"
+  },
+  {
+    "revision": "d27cd6a757e7ee70d726",
+    "url": "/recipe-notebook/static/js/3.d4729402.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/recipe-notebook/static/js/3.d4729402.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "609a98a00b472908672d",
+    "url": "/recipe-notebook/static/js/4.137038df.chunk.js"
+  },
+  {
+    "revision": "fcd26faaeed958a0506a",
+    "url": "/recipe-notebook/static/js/5.82b12c46.chunk.js"
+  },
+  {
+    "revision": "4b629ffb6c0cbd0b66d8",
+    "url": "/recipe-notebook/static/js/6.a72142ee.chunk.js"
+  },
+  {
+    "revision": "506158087ce7e160d0fe",
+    "url": "/recipe-notebook/static/js/7.21f03005.chunk.js"
+  },
+  {
+    "revision": "cb72a48ec2fbb66295d0",
+    "url": "/recipe-notebook/static/js/main.7ad12f73.chunk.js"
+  },
+  {
+    "revision": "cd4d0e19794a35861baf",
+    "url": "/recipe-notebook/static/js/runtime-main.478847dc.js"
+  },
+  {
+    "revision": "4209646228e341638ad2bc46bb6b0e6c",
+    "url": "/recipe-notebook/static/media/Cranberry_sauce.42096462.jpg"
+  },
+  {
+    "revision": "1fdde7e8a3830ecb06eee81469fac329",
+    "url": "/recipe-notebook/static/media/Eggplant_parmesan.1fdde7e8.jpg"
+  },
+  {
+    "revision": "4486347ca300f396e66aa1fa285f8ead",
+    "url": "/recipe-notebook/static/media/Sweet_potato_burritos.4486347c.jpg"
+  },
+  {
+    "revision": "1e0e7fea887722d0562f34fefc74a38a",
+    "url": "/recipe-notebook/static/media/crustless_spinach_quiche.1e0e7fea.jpg"
+  }
+]);
