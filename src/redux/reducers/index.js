8f8f1b44a3fc5d0a5e5a0ce62af0b5a3eccdcc8d
Zack Koh <zhenxiang.koh@gmail.com>
2020-03-19 22:54:47 +0800

    combine reducers

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+
+import recipeReducer from "./recipeReducer";
+import userReducer from "./userReducer";
+
+export default combineReducers({ recipes: recipeReducer, user: userReducer});
